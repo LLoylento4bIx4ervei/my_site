8e3279ea728aff28df426d637dfc414d3e1c9530
Stas <kil-stas@rambler.ru>
2021-12-03 17:49:02 +0300

    I add some data in views.py

--- my_site/views.py
+++ my_site/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render
 
-
-
-
 info = [
     {
         'name': 'Stas',
         'age': 23,
         'content': 'My little site',
-        'date_posted': '01.12.2021'
+        'date_posted': '02.12.2021'
     }
 
 ]
 
 
-
-
 def home(request):
-    return render(request, 'my_site/home_page.html')
+    return render(request, 'my_site/home_page.html', {'title': 'Home Page'})
+
 
 def about_author(request):
-    post = {'info':info}
-    return render(request,'my_site/about.html', post)
+    post = {'info': info}
+    return render(request, 'my_site/about.html', post)
