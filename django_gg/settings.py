09c5da6fa051b617089f32921e8ddf7f77d8f2a6
Stas <kil-stas@rambler.ru>
2021-12-04 14:58:17 +0300

    Í add users app for registration

--- django_gg/settings.py
+++ django_gg/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'my_site.apps.MySiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- my_site/views.py
+++ my_site/views.py
@@ -2,15 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.models import User
 
-info = [
-    {
-        'name': 'Stas',
-        'age': 23,
-        'content': 'My little site',
-        'date_posted': '02.12.2021'
-    }
 
-]
 
 
 def home(request):
