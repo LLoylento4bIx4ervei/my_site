239fcc66fad0e805e4957f090abbbaa6db03e2ec
Stas <kil-stas@rambler.ru>
2021-12-04 20:29:58 +0300

    I add simple test

--- my_site/tests.py
+++ my_site/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import models
+from http import HTTPStatus
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+
+    def test_page_is_ok_register(self):
+        response = self.c.get('/register', follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+
+
+
+
